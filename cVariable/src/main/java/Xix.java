8f6d6acdcaeff5d4beb59223338c92b8a50a5ebc
Wei Ding <ding_web@yahoo.co.jp>
2020-01-30 01:43:47 -0500

    print variables

--- cVariable/src/main/java/Xix.java
+++ cVariable/src/main/java/Xix.java
@@ -6,14 +6,14 @@ public class Xix {
 
     public static void method(){
         str = "sss";
-        System.out.println("This is a static method not relative with any object");
+        System.out.println("This is a static method not relative with any object. str [" + str + "]");
     }
 
     public void method2(){
         str = "sss_sss01";
         str2 = "sss_sss02";
         method();
-        System.out.println("This is a non-static method.");
+        System.out.println("This is a non-static method. str [" + str + "] str2 [" + str2 + "]");
     }
 
     public static void main(String[] args){
