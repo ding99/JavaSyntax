66713a5024979926778b1308f72e206d66cfc6d1
Wei Ding <ding_web@yahoo.co.jp>
2020-08-23 23:45:09 -0400

    hashset

--- cVariable/src/main/java/PracticeTime.java
+++ cVariable/src/main/java/PracticeTime.java
@@ -8,6 +8,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashSet;
 
 public class PracticeTime {
     void Start(){
@@ -20,6 +21,7 @@ public class PracticeTime {
         System.out.println(date.format(DateTimeFormatter.ofPattern("E, MMM dd yyyy")));
 
         Lists();
+        HashSets();
     }
 
     void Lists(){
@@ -27,6 +29,19 @@ public class PracticeTime {
         List<Integer> list = new ArrayList<Integer>();
         list.add(5);list.add(15);list.add(25);
         for(Integer i : list) System.out.print(i  + " ");
+        System.out.println();
+    }
+
+    void HashSets(){
+        System.out.println("=> Practice HashSet");
+        HashSet<String> cars = new HashSet<>();
+        cars.add("BMW"); cars.add("Volvo"); cars.add("Mazda");
+        for(String c : cars) System.out.print(c + " ");
+        System.out.println();
+
+        System.out.println("Volvo existing: (" + cars.contains("Volvo") + ")");
+        System.out.println("Remove Volvo"); cars.remove("Volvo");
+        for(String c : cars) System.out.print(c + " ");
         System.out.println();
     }
 }
