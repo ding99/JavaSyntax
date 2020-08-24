5a7735d7e0a5dfc212fb4d680c61c7fbc0684afc
Wei Ding <ding_web@yahoo.co.jp>
2020-08-23 23:18:02 -0400

    arraylist

--- cVariable/src/main/java/PracticeTime.java
+++ cVariable/src/main/java/PracticeTime.java
@@ -6,6 +6,8 @@ package main.java;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PracticeTime {
     void Start(){
@@ -16,5 +18,15 @@ public class PracticeTime {
 
         System.out.println(date.format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")));
         System.out.println(date.format(DateTimeFormatter.ofPattern("E, MMM dd yyyy")));
+
+        Lists();
+    }
+
+    void Lists(){
+        System.out.println("=> Practice ArrayList");
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(5);list.add(15);list.add(25);
+        for(Integer i : list) System.out.print(i  + " ");
+        System.out.println();
     }
 }
