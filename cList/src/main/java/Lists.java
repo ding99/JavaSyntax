71931a25d9605509ae0c60025775fe56d99e5340
Wei Ding <ding_web@yahoo.co.jp>
2020-09-20 19:20:15 -0400

    linkedlist

--- cList/src/main/java/Lists.java
+++ cList/src/main/java/Lists.java
@@ -7,16 +7,31 @@ package main.java;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 
 public class Lists {
 
     public void Start(){
         AList();
+        LinkList();
+    }
+
+    private void LinkList(){
+        System.out.println("-- LinkedList");
+        LinkedList<String> cars = new LinkedList<>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        System.out.println(cars);
+        System.out.println("first <" + cars.getFirst() + "> last <" + cars.getLast() + ">");
+        Collections.sort(cars);
+        System.out.println("first <" + cars.getFirst() + "> last <" + cars.getLast() + ">");
     }
 
     private void AList(){
         System.out.println("-- ArrayList");
-        List<String> cars = new ArrayList<>();
+        ArrayList<String> cars = new ArrayList<>();
         cars.add("Volvo");
         cars.add("BMW");
         cars.add("Ford");
