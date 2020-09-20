77d47fe3d74dd29887f41894beb93fcf03789024
Wei Ding <ding_web@yahoo.co.jp>
2020-09-20 15:53:21 -0400

    formatter

--- cTime/src/main/java/ExamineTime.java
+++ cTime/src/main/java/ExamineTime.java
@@ -4,19 +4,18 @@ package main.java;
  * Created by ding_ on 9/20/2020
  */
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.LocalDateTime;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 
 public class ExamineTime {
     public void Start(){
         LocalDate date = LocalDate.now();
-        System.out.println(date);
-
         LocalTime time = LocalTime.now();
-        System.out.println(time);
+        System.out.println("Data: " + date + ", Time: " + time);
 
         LocalDateTime datetime = LocalDateTime.now();
-        System.out.println(datetime);
+        System.out.println("Before formatting: " + datetime);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        System.out.println("After  formatting: " + formatter.format(datetime));
     }
 }
