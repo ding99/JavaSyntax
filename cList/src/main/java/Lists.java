b43a48ed276236c6f5c927097a33f0ebe31a93d8
Wei Ding <ding_web@yahoo.co.jp>
2020-09-20 19:11:45 -0400

    arraylist

--- cList/src/main/java/Lists.java
+++ cList/src/main/java/Lists.java
@@ -4,8 +4,25 @@ package main.java;
  * Created by ding_ on 9/20/2020
  */
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Lists {
+
     public void Start(){
-        System.out.println("-- Sub Class Lists");
+        AList();
+    }
+
+    private void AList(){
+        System.out.println("-- ArrayList");
+        List<String> cars = new ArrayList<>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        System.out.println(cars);
+        Collections.sort(cars);
+        System.out.println(cars);
     }
 }
