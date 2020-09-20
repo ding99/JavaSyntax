547e00a9917192af5461cee8fdaf01849cceb9c6
Wei Ding <ding_web@yahoo.co.jp>
2020-09-20 15:45:11 -0400

    entrance for time examination

--- cTime/src/main/java/ExamineTime.java
+++ cTime/src/main/java/ExamineTime.java
@@ -5,10 +5,18 @@ package main.java;
  */
 
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.LocalDateTime;
 
 public class ExamineTime {
-    public static void main(String[] args){
+    public void Start(){
         LocalDate date = LocalDate.now();
         System.out.println(date);
+
+        LocalTime time = LocalTime.now();
+        System.out.println(time);
+
+        LocalDateTime datetime = LocalDateTime.now();
+        System.out.println(datetime);
     }
 }
