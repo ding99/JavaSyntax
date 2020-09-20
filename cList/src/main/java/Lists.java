e084d4b002efa3fc6bf403d6a5565d4d3fda2294
Wei Ding <ding_web@yahoo.co.jp>
2020-09-20 19:32:04 -0400

    hashmap

--- cList/src/main/java/Lists.java
+++ cList/src/main/java/Lists.java
@@ -4,16 +4,33 @@ package main.java;
  * Created by ding_ on 9/20/2020
  */
 
-import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
+import java.util.HashMap;
 
 public class Lists {
 
     public void Start(){
         AList();
         LinkList();
+        HMap();
+    }
+
+    private void HMap(){
+        System.out.println("-- HashMap");
+        HashMap<String,String> cities = new HashMap<>();
+        cities.put("England","London");
+        cities.put("Germany","Berlin");
+        cities.put("Norway","London");
+        cities.put("USA","Washington DC");
+        System.out.println(cities + ", size " + cities.size());
+        System.out.println("Norway <" + cities.get("Norway") + ">");
+        cities.remove("Germany");
+        System.out.println(cities + ", size " + cities.size());
+
+        for(String i : cities.keySet())
+            System.out.println("  <" + i + "> value <" + cities.get(i) + ">");
     }
 
     private void LinkList(){
