c25f3b0f6576d31e16842c4862d485cf5a0dda27
Wei Ding <ding_web@yahoo.co.jp>
2020-09-20 14:58:25 -0400

    start time examination

---
+++ cTime/src/main/java/ExamineTime.java
@@ -0,0 +1,14 @@
+package main.java;
+
+/**
+ * Created by ding_ on 9/20/2020
+ */
+
+import java.time.LocalDate;
+
+public class ExamineTime {
+    public static void main(String[] args){
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
+    }
+}
