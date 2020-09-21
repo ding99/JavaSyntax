1da238f01d7d78a7f5467a25af3b9dcbd1e226f7
Wei Ding <ding_web@yahoo.co.jp>
2020-09-20 22:50:51 -0400

    write file

--- cFile/src/main/java/Filing.java
+++ cFile/src/main/java/Filing.java
@@ -5,15 +5,46 @@ package main.java;
  */
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Filing {
+    String name = "d:\\test\\testJava\\testwrite01.txt";
+
     public void Start(){
         System.out.println("-- start Filing");
         Creating();
+        Writing();
+    }
+
+    private void Writing(){
+        System.out.println("-- Write file");
+        try{
+            FileWriter writer = new FileWriter(name);
+            writer.write("Files in Java might be tricky, but is is fun enough!");
+            writer.close();
+            System.out.println("Successfully wrote to the file");
+        } catch (IOException e){
+            System.out.println("An error occurred.");
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     private void Creating(){
-        System.out.println("-- Create File");
+        System.out.println("-- Create file");
+
+        try {
+            File file = new File(name);
+            if(file.createNewFile()) {
+                System.out.println("File created: " + file.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch(IOException e){
+            System.out.println("An error occurred.");
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
