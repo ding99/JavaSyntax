68c0f69a20abc17e8353a5f2dbdf30808030e011
Wei Ding <ding_web@yahoo.co.jp>
2020-09-21 18:12:57 -0400

    start regex module

---
+++ cRegEx/src/main/java/ExamineRegEx.java
@@ -0,0 +1,11 @@
+package main.java;
+
+/**
+ * Created by ding_ on 9/21/2020
+ */
+
+public class ExamineRegEx {
+    public void Start(){
+        System.out.println("Examining Regular Expression");
+    }
+}
