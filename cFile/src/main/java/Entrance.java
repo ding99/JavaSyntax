c69d788610605241355a2c5fcca43b018d6a1fac
Wei Ding <ding_web@yahoo.co.jp>
2020-09-20 22:38:07 -0400

    start filing

---
+++ cFile/src/main/java/Entrance.java
@@ -0,0 +1,13 @@
+package main.java;
+
+/**
+ * Created by ding_ on 9/20/2020
+ */
+
+public class Entrance {
+    public static void main(String[] args){
+        System.out.println("== Start");
+        new Filing().Start();
+        System.out.println("== End");
+    }
+}
---
+++ cFile/src/main/java/Filing.java
@@ -0,0 +1,19 @@
+package main.java;
+
+/**
+ * Created by ding_ on 9/20/2020
+ */
+
+import java.io.File;
+import java.io.IOException;
+
+public class Filing {
+    public void Start(){
+        System.out.println("-- start Filing");
+        Creating();
+    }
+
+    private void Creating(){
+        System.out.println("-- Create File");
+    }
+}
