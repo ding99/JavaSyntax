27edabeb825d0f9e2ac924f3fb844b0ec9ab1884
Wei Ding <ding_web@yahoo.co.jp>
2020-09-23 13:39:27 -0400

    threads module

---
+++ cThreads/src/main/java/Entrance.java
@@ -0,0 +1,11 @@
+package main.java;
+
+/**
+ * Created by ding_ on 9/23/2020
+ */
+public class Entrance {
+    public static void main(String[] args){
+        System.out.println("== Module Multi Threads : Start");
+        System.out.println("== End");
+    }
+}
